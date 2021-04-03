39a5e7674975184a7a6c7f2e935da3d883a8254b
janu8ry <janu8ry0108@gmail.com>
2021-04-03 10:55:05 +0900

    minor fix

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const colors = {
     brand: {
         900: '#1a365d',
         800: '#153e75',
-        700: '#2a69ac',
-    },
+        700: '#2a69ac'
+    }
 };
 const theme = extendTheme({ colors });
 
@@ -24,6 +24,7 @@ function App() {
                 <Route path="/social" component={Social} />
                 <Route component={NotFound} />
             </Switch>
+            {/* todo: footer */}
         </ChakraProvider>
     );
 }
