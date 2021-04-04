71f88a676f8bcc431f03335c2f0b8b67cea8bc86
janu8ry <janu8ry0108@gmail.com>
2021-04-05 00:12:43 +0900

    delete workbox config

--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    globDirectory: 'build/',
-    globPatterns: ['**/*.{json,ico,png,txt,js,xml,svg}'],
-    swDest: 'build/service-worker.js',
-    ignoreURLParametersMatching: ['/^utm_/', '/^fbclid$/']
-};
